b3f8fc6044004e23c3e914deff8ba43463f850fe
Nick Adams <nick.pappas.adams@gmail.com>
2022-10-11 15:02:32 -0700

    added delete request

---
+++ client/src/APIHooks/DELETE.js
@@ -0,0 +1,12 @@
+
+
+const Delete = (url, id) => {
+    fetch(`${url}/${id}`, {
+        method: "DELETE"
+
+    })
+    .then(() => console.group("Item Deleted!!!"))
+    .catch(err => console.log(err))
+}
+
+export default Delete
