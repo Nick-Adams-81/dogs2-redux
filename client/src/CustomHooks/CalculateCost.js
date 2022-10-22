f1c799734b47ba0f8297837d8db1599439f52410
Nick Adams <nick.pappas.adams@gmail.com>
2022-10-22 14:37:58 -0700

    finished formatting files

--- client/src/CustomHooks/CalculateCost.js
+++ client/src/CustomHooks/CalculateCost.js
@@ -1,5 +1,4 @@
-
 const CalculateCost = (itemCost, itemAmmount) => {
-    return (itemCost * itemAmmount).toFixed(2)
-}
-export default CalculateCost
+  return (itemCost * itemAmmount).toFixed(2);
+};
+export default CalculateCost;
